1c059cead8620e73b069e99024b77a9962c91c40
James Jiang <monochrome473@gmail.com>
2020-03-21 20:35:47 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,7 @@
-# Write your #display_rainbow method here
+def display_rainbow(colors)
+  puts "R: #{colors(0)}, O: #{colors(1)}, Y: #{colors(2)}"
+end
+
+colors = [red, orange, yellow]
+
+puts display_rainbow
