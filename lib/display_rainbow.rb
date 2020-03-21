cb52c0910ce044318349e01e1557fa90e4e7f5f2
James Jiang <monochrome473@gmail.com>
2020-03-21 21:09:02 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,4 @@
 colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
 def display_rainbow
-  puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}
+  puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}"
 end
